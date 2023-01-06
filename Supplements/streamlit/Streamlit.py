7076a124289bf19ae1ca942b25c2310551b9568b
Skye Jeanat <sjeanat3@gmail.com>
2023-01-06 12:14:57 -0500

    fix streamlit

--- Supplements/streamlit/Streamlit.py
+++ Supplements/streamlit/Streamlit.py
@@ -15,12 +15,12 @@ from sklearn.compose import ColumnTransformer
 from geopy.distance import geodesic
 from datetime import timedelta, date
 
-asheville_modeling_data = pd.read_pickle('../streamlit/asheville_modeling_data.pkl')
+# asheville_modeling_data = pd.read_pickle('../streamlit/asheville_modeling_data.pkl')
 # asheville_modeling_data = open('../Pickles/asheville_modeling_data.pickle', 'wb')
-# with open('../Pickles/asheville_modeling_data.pickle', 'rb') as f:
+with open('asheville_modeling_data.pkl', 'rb') as f:
+    asheville_modeling_data = pickle.load(f)
 # asheville_modeling_data = pickle.load(asheville_modeling_data)
-# asheville_modeling_data = pickle.load(asheville_modeling_data)
-streamlit_model3_results = joblib.load('../streamlit/streamlit_model3_results.joblib')
+streamlit_model3_results = joblib.load('streamlit_model3_results.joblib')
 
 st.set_page_config(layout="wide")
 st.sidebar.header('Please Specify Your AirBnB Characteristics:')
